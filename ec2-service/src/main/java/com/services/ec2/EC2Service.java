42b61fa348680686e715373b92f73dad7b2eb924
Seth Theeke <Seth.P.Theeke@leidos.com>
2017-11-11 16:40:19 -0500

    Started AmazonEC2 Service

---
+++ ec2-service/src/main/java/com/services/ec2/EC2Service.java
@@ -0,0 +1,10 @@
+package com.services.ec2;
+
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+
+public class EC2Service {
+
+	public void createEC2Instance() {
+		RunInstancesRequest runInstancesRequest = new RunInstancesRequest();
+	}
+}
